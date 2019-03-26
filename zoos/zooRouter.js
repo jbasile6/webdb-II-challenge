ea4ea72bb8c2fc22c842ea6c8e31b929fc0b0f8a
James Basile <jbasile145@gmail.com>
2019-03-26 16:04:26 -0400

    GET zoo by id endpoint built, db array is empty so cannot test yet, error message is working

--- zoos/zooRouter.js
+++ zoos/zooRouter.js
@@ -13,9 +13,27 @@ const db = knex(knexConfig)
 
 // endpoints here
 
+//GET all zoos
 router.get('/', (req, res) => {
     db('zoos')
         .then( zoos => res.status(200).json(zoos))
+        .catch( err => res.status(500).json(err));
+});
+
+//GET zoos by id
+router.get('/:id', (req, res) => {
+    const zooId = req.params.id;
+
+    db('zoos')
+        .where({ id: zooId })
+        .first()
+        .then( zoo => {
+            if (!zoo) {
+                res.status(404).json({ error: `Zoo id #${zooId} does not exist` })
+            } else {
+                res.status(200).json(zoo)
+            }
+        })
         .catch( err => res.status(500).json(err));
 });
 
